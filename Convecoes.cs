cb1fbf88bed87c401377f3ec96b8942e58abfadb
Leticia Rodrigues <71568815+leticia-rodrigues2@users.noreply.github.com>
2023-12-12 20:58:00 -0300

    Restrições e convenções para nomes

---
+++ Convecoes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Convencoes 
+{
+    internal class ContaBancaria
+    {
+        public string Titular {  get; set; }
+        private double _saldo;
+        public void Deposito (double quantia)
+        {
+            _saldo += quantia;
+        }
+        public double GetSaldo() 
+        {
+            return _saldo;
+        }
+    }
+
+    //Camel Case: (parâmetros de métodos, variáveis de métodos)
+
+    // Pascal Case: LastName(namespaces, classes, properties e métodos)
+
+    //Padrão _lastName: (atributos "internos" da classe)
+}
